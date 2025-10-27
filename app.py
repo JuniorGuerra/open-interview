d16ebc01011c6ad6bf6965526dbc23df61b4239d
JuniorGuerra <juniorguerrac17@gmail.com>
2025-10-27 18:04:49 -0500

    wip

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def visits_api():
 def health():
     try:
         redis.ping()
+        print("Health check passed.")
         return jsonify({'status': 'healthy'}), 200
     except:
         return jsonify({'status': 'unhealthy'}), 500
